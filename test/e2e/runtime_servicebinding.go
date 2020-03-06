6a0db1b57c6b648136002cf28a7caaf4747af1e7
Ethan Johnston <ethan.johnston@ibm.com>
2020-03-06 16:43:21 -0500

    Init the service binding e2e testing

---
+++ test/e2e/runtime_servicebinding.go
@@ -0,0 +1,33 @@
+package e2e
+
+import (
+	"testing"
+
+	"github.com/application-stacks/operator/test/util"
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	e2eutil "github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+)
+
+func RuntimeServiceBindingTest(t *testing.T) {
+	ctx, err := util.InitializeContext(t, cleanupTimeout, retryInterval)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ctx.Cleanup()
+
+	ns, err := ctx.GetNamespace()
+	if err != nil {
+		t.Fatalf("Couldn't get namespace: %v", err)
+	}
+
+	t.Logf("Namespace: %s", ns)
+
+	f := framework.Global
+
+	err = e2eutil.WaitForOperatorDeployment(t, f.KubeClient, ns, "application-stacks-operator", 1, retryInterval, timeout)
+	if err != nil {
+		util.FailureCleanup(t, f, ns, err)
+	}
+
+
+}
