5dffa10df94fb40d13f921f5d7f6fe5ede78a80d
Ethan Johnston <ethan.johnston@ibm.com>
2020-06-25 12:56:38 -0400

    test larger number of goroutines

--- test/e2e/runtime_test.go
+++ test/e2e/runtime_test.go
@@ -11,6 +11,22 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type Test struct {
+	Test func(*testing.T)
+	Name string
+}
+
+var tests []Test = []Test{
+	{Name: "RuntimePullPolicyTest", Test: RuntimePullPolicyTest},
+	{Name: "RuntimeBasicTest", Test: RuntimeBasicTest},
+	{Name: "RuntimeProbeTest", Test: RuntimeProbeTest},
+	{Name: "RuntimeAutoScalingTest", Test: RuntimeAutoScalingTest},
+	{Name: "RuntimeBasicStorageTest", Test: RuntimeBasicStorageTest},
+	{Name: "RuntimePersistenceTest", Test: RuntimePersistenceTest},
+}
+
+// NOTE: on the use of goroutines, concurrency puts a strain on the 3.11 cluster
+// in particular on the docker registry. Otherwise each test could run at the same time.
 // TestRuntimeComponent ... end to end tests
 func TestRuntimeComponent(t *testing.T) {
 	runtimeComponentList := &appstacksv1beta1.RuntimeComponentList{
@@ -45,27 +61,34 @@ func TestRuntimeComponent(t *testing.T) {
 		}
 	}
 }
+
+// Long running tests that should be in routines:
+// ServiceBindingTest - 258
+// AutoScalingTest - 163
+// CertManager - 93
 func testBasicFeatures(t *testing.T) {
-	t.Run("RuntimePullPolicyTest", RuntimePullPolicyTest)
-	t.Run("RuntimeBasicTest", RuntimeBasicTest)
-	t.Run("RuntimeProbeTest", RuntimeProbeTest)
-	t.Run("RuntimeAutoScalingTest", RuntimeAutoScalingTest)
-	t.Run("RuntimeStorageTest", RuntimeBasicStorageTest)
-	t.Run("RuntimePersistenceTest", RuntimePersistenceTest)
+	// t.Run("RuntimePullPolicyTest", RuntimePullPolicyTest)
+	// t.Run("RuntimeBasicTest", RuntimeBasicTest)
+	// t.Run("RuntimeProbeTest", RuntimeProbeTest)
+	// // This test is long, create go routine
+	// go t.Run("RuntimeAutoScalingTest", RuntimeAutoScalingTest)
+	// t.Run("RuntimeStorageTest", RuntimeBasicStorageTest)
+	// t.Run("RuntimePersistenceTest", RuntimePersistenceTest)
+	for _, test := range tests {
+		go t.Run(test.Name, test.Test)
+	}
 }
 
 func testAdvancedFeatures(t *testing.T) {
 	// These features require a bit of configuration
 	// which makes them less ideal for quick minikube tests
 
-	// split tests in half to reduce build time.
-	go func() {
-		t.Run("RuntimeServiceMonitorTest", RuntimeServiceMonitorTest)
-		t.Run("RuntimeKnativeTest", RuntimeKnativeTest)
-	}()
+	// create routines for the longest tests
+	go t.Run("RuntimeServiceBindingTest", RuntimeServiceBindingTest)
+	go t.Run("RuntimeCertManagerTest", RuntimeCertManagerTest)
 
-	t.Run("RuntimeServiceBindingTest", RuntimeServiceBindingTest)
-	t.Run("RuntimeCertManagerTest", RuntimeCertManagerTest)
+	t.Run("RuntimeKnativeTest", RuntimeKnativeTest)
+	t.Run("RuntimeServiceMonitorTest", RuntimeServiceMonitorTest)
 }
 
 // Verify functionality that is tied to OCP
