1cec55cc5c76135e55dbb76656c64b73fdb714bb
Ethan Johnston <ethan.johnston@ibm.com>
2020-07-16 14:14:14 -0400

    Fix knative installation and tests

--- test/util/util.go
+++ test/util/util.go
@@ -382,9 +382,14 @@ func CommandError(t *testing.T, err error, out []byte) error {
 	return nil
 }
 
-func isKnativeInstalled(t *testing.T, f *framework.Framework) error {
+func IsKnativeInstalled(t *testing.T, f *framework.Framework) error {
 	ksvc := servingv1alpha1.Service{}
-	err := f.Client.List(goctx.TODO(), &ksvc)
+	err := servingv1alpha1.AddToScheme(f.Scheme)
+	if err != nil {
+		return err
+	}
+	
+	err = f.Client.List(goctx.TODO(), &ksvc)
 	if err != nil {
 		t.Log(err)
 		return err
