9d2269d19ab9593395972fd87261c1af2d8b2b72
Hank W Ibell <hwibell@us.ibm.com>
2021-10-11 14:03:49 -0400

    Enable e2e test

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,52 +1,118 @@
 #!/bin/bash
 
+readonly usage="Usage: e2e.sh -u <docker-username> -p <docker-password> --cluster-url <url> --cluster-token <token> --registry-name <name> --registry-namespace <namespace>"
 readonly SERVICE_ACCOUNT="travis-tests"
+readonly OC_CLIENT_VERSION="4.6.0"
 
-# login_cluster : Download oc cli and use it to log into our persistent cluster
-login_cluster(){
+# setup_env: Download oc cli, log into our persistent cluster, and create a test project
+setup_env() {
+    echo "****** Installing OC CLI..."
     # Install kubectl and oc
-    curl -L https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-4.3/openshift-client-linux.tar.gz | tar xvz
+    curl -L https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OC_CLIENT_VERSION}/openshift-client-linux.tar.gz | tar xvz
     sudo mv oc kubectl /usr/local/bin/
+
     # Start a cluster and login
-    oc login ${OC_URL} --token=${OC_TOKEN}
+    echo "****** Logging into remote cluster..."
+    oc login "${OC_URL}" --token="${OC_TOKEN}"
+
     # Set variables for rest of script to use
     readonly DEFAULT_REGISTRY=$(oc get route "${REGISTRY_NAME}" -o jsonpath="{ .spec.host }" -n "${REGISTRY_NAMESPACE}")
-    readonly BUILD_IMAGE=${DEFAULT_REGISTRY}/openshift/runtime-operator:${TRAVIS_BUILD_NUMBER}
+    readonly TEST_NAMESPACE="runtime-operator-test-${TRAVIS_BUILD_NUMBER}"
+    readonly BUILD_IMAGE=${DEFAULT_REGISTRY}/${TEST_NAMESPACE}/runtime-operator
+    readonly BUNDLE_IMAGE="${DEFAULT_REGISTRY}/${TEST_NAMESPACE}/rco-bundle:latest"
+
+    echo "****** Creating test namespace: ${TEST_NAMESPACE}"
+    oc new-project "${TEST_NAMESPACE}"
+
+    ## Create service account for Kuttl tests
+    oc apply -f config/rbac/kuttl-rbac.yaml
 }
 
-## cleanup : Delete generated resources that are not bound to a test namespace.
-cleanup() {
-    # Remove image related resources after the test has finished
-    oc delete imagestream "runtime-operator:${TRAVIS_BUILD_NUMBER}" -n openshift
+## cleanup_env : Delete generated resources that are not bound to a test TEST_NAMESPACE.
+cleanup_env() {
+  oc delete project "${TEST_NAMESPACE}"
+}
+
+push_images() {
+    echo "****** Logging into private registry..."
+    oc sa get-token "${SERVICE_ACCOUNT}" -n default | docker login -u unused --password-stdin "${DEFAULT_REGISTRY}" || {
+        echo "Failed to log into docker registry as ${SERVICE_ACCOUNT}, exiting..."
+        exit 1
+    }
+
+    echo "****** Creating pull secret using Docker config..."
+    oc create secret generic regcred --from-file=.dockerconfigjson="${HOME}/.docker/config.json" --type=kubernetes.io/dockerconfigjson
+
+    docker push "${BUILD_IMAGE}" || {
+        echo "Failed to push ref: ${BUILD_IMAGE} to docker registry, exiting..."
+        exit 1
+    }
+
+    docker push "${BUNDLE_IMAGE}" || {
+        echo "Failed to push ref: ${BUNDLE_IMAGE} to docker registry, exiting..."
+        exit 1
+    }
 }
 
 main() {
     parse_args "$@"
-    echo "****** Logging into remote cluster..."
-    login_cluster
-    echo "****** Logging into private registry..."
-    echo $(oc sa get-token travis-tests -n default) | docker login -u unused --password-stdin $DEFAULT_REGISTRY
 
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to log into docker registry as ${SERVICE_ACCOUNT}, exiting..."
+    if [[ -z "${USER}" || -z "${PASS}" ]]; then
+        echo "****** Missing docker authentication information, see usage"
+        echo "${usage}"
         exit 1
     fi
+
+    if [[ -z "${OC_URL}" ]] || [[ -z "${OC_TOKEN}" ]]; then
+        echo "****** Missing OCP URL or token, see usage"
+        echo "${usage}"
+        exit 1
+    fi
+
+    if [[ -z "${REGISTRY_NAME}" ]] || [[ -z "${REGISTRY_NAMESPACE}" ]]; then
+        echo "****** Missing OCP registry name or registry namespace, see usage"
+        echo "${usage}"
+        exit 1
+    fi
+
+    echo "****** Setting up test environment..."
+    setup_env
+
+    ## login to docker to avoid rate limiting during build
+    echo "${PASS}" | docker login -u "${USER}" --password-stdin
 
     echo "****** Building image"
-    operator-sdk build "${BUILD_IMAGE}"
-    echo "****** Pushing image into registry..."
-    docker push "${BUILD_IMAGE}"
+    docker build -t "${BUILD_IMAGE}" .
 
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to push ref: ${BUILD_IMAGE} to docker registry, exiting..."
+    echo "****** Building bundle..."
+    IMG="${BUILD_IMAGE}" BUNDLE_IMG="${BUNDLE_IMAGE}" make kustomize bundle bundle-build
+
+    echo "****** Pushing operator and operator bundle images into registry..."
+    push_images
+
+    echo "****** Installing bundle..."
+    operator-sdk run bundle --install-mode OwnNamespace --pull-secret-name regcred "${BUNDLE_IMAGE}" || {
+        echo "****** Installing bundle failed..."
         exit 1
-    fi
+    }
 
-    echo "****** Starting e2e tests..."
-    CLUSTER_ENV="ocp" operator-sdk test local github.com/application-stacks/runtime-component-operator/test/e2e --debug --verbose  --go-test-flags "-timeout 35m" --image $(oc registry info)/openshift/runtime-operator:$TRAVIS_BUILD_NUMBER
+    # Wait for operator deployment to be ready
+    while [[ $(oc get deploy rco-controller-manager -o jsonpath='{ .status.readyReplicas }') -ne "1" ]]; do
+        echo "****** Waiting for rco-controller-manager to be ready..."
+        sleep 10
+    done
+
+    echo "****** rco-controller-manager deployment is ready..."
+
+    echo "****** Starting scorecard tests..."
+    operator-sdk scorecard --verbose --selector=suite=kuttlsuite --namespace "${TEST_NAMESPACE}" --service-account scorecard-kuttl --wait-time 30m ./bundle || {
+        echo "****** Scorecard tests failed..."
+        exit 1
+    }
     result=$?
-    echo "****** Cleaning up tests..."
-    cleanup
+
+    echo "****** Cleaning up test environment..."
+    cleanup_env
 
     return $result
 }
@@ -54,6 +120,14 @@ main() {
 parse_args() {
     while [ $# -gt 0 ]; do
     case "$1" in
+    -u)
+      shift
+      readonly USER="${1}"
+      ;;
+    -p)
+      shift
+      readonly PASS="${1}"
+      ;;
     --cluster-url)
       shift
       readonly OC_URL="${1}"
@@ -79,6 +153,5 @@ parse_args() {
     shift
   done
 }
-
 
 main "$@"
