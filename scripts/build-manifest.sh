40be88619ac2713c724d32ec30c9c2bfc137544c
Hank Ibell <hwibell@gmail.com>
2021-11-23 15:59:06 -0500

    Temporarily remove other platforms from manifest (#279)

--- scripts/build-manifest.sh
+++ scripts/build-manifest.sh
@@ -59,8 +59,10 @@ build_manifest() {
   ## try to build manifest but allow failure
   ## this allows new release builds
   local target="${IMAGE}:${tag}"
+  # TODO: Add back in linux/s390x and linux/ppc64le once build platforms are back up
+  # --platforms "linux/amd64,linux/s390x,linux/ppc64le" \
   manifest-tool push from-args \
-    --platforms "linux/amd64,linux/s390x,linux/ppc64le" \
+    --platforms "linux/amd64" \
     --template "${target}-ARCH" \
     --target "${target}" \
     || echo "*** WARN: Target architectures not available"
