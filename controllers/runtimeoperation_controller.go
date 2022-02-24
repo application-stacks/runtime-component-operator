9fd5ea89ddf28971149041c87fbf5bee4965a0a8
Jason Yong <jason_yong@uk.ibm.com>
2022-02-24 10:38:20 +0000

    Update RBAC persmissions for rco-manager-role
    Signed-off-by: Jason Yong <jason_yong@uk.ibm.com>

--- controllers/runtimeoperation_controller.go
+++ controllers/runtimeoperation_controller.go
@@ -49,7 +49,7 @@ type RuntimeOperationReconciler struct {
 	RestConfig *rest.Config
 }
 
-// +kubebuilder:rbac:groups=rc.app.stacks,resources=runtimeoperations;runtimeoperations/status;runtimeoperations/finalizers,verbs=get;list;watch,namespace=runtime-component-operator
+// +kubebuilder:rbac:groups=rc.app.stacks,resources=runtimeoperations;runtimeoperations/status;runtimeoperations/finalizers,verbs=get;list;update;watch,namespace=runtime-component-operator
 // +kubebuilder:rbac:groups=core,resources=pods;pods/exec,verbs=list;watch;create;update;delete,namespace=runtime-component-operator
 
 func (r *RuntimeOperationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
