cca639878e7f9ff07a7d9b61f5dc003db6bc2b9d
Melissa <halim.lee@ibm.com>
2023-04-14 07:41:47 -0700

    Update k8s.gcr.io to registry.k8s.io

--- scripts/e2e-kind.sh
+++ scripts/e2e-kind.sh
@@ -7,7 +7,7 @@ readonly BUILD_IMAGE="runtime-component-operator:latest"
 
 readonly RUNASUSER="\n  securityContext:\n    runAsUser: 1001"
 readonly APPIMAGE='applicationImage:\s'
-readonly IMAGE='k8s.gcr.io\/pause:3.2'
+readonly IMAGE='registry.k8s.io\/pause:3.2'
 
 main() {
     parse_args "$@"
