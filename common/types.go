ba135b33e05c5a443d9b44f4646414e127cc3850
ilewis <idlewis@users.noreply.github.com>
2023-11-14 16:44:51 +0000

    Add config to allow setting enableServiceLinks

--- common/types.go
+++ common/types.go
@@ -230,4 +230,5 @@ type BaseComponent interface {
 	GetTopologySpreadConstraints() BaseComponentTopologySpreadConstraints
 	GetSecurityContext() *corev1.SecurityContext
 	GetManageTLS() *bool
+	GetEnableServiceLinks() *bool
 }
