cf8dced8d4556b06422b3310dc18e64c678aaf35
ilewis <idlewis@users.noreply.github.com>
2024-03-12 16:04:28 +0000

    Put stuff in a function

--- controllers/runtimecomponent_controller.go
+++ controllers/runtimecomponent_controller.go
@@ -432,23 +432,13 @@ func (r *RuntimeComponentReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		r.ManageError(err, common.StatusConditionTypeReconciled, instance)
 	} else if ok {
 		if instance.Spec.Expose != nil && *instance.Spec.Expose {
-
-			// If a custome hostname was previously set, but is now not set, any previous
-			// route needs to be deleted, as the host in a route cannot be unset
-			// and the default generated hostman is difficult to manually recreate
-			rh := ba.GetStatus().GetReferences()[common.StatusReferenceRouteHost]
-			if rh != "" {
-				// The host was previously set.
-				// If the host is now empty, delete the old route
-				rt := ba.GetRoute()
-				if rt == nil || (rt.GetHost() == "" && common.Config[common.OpConfigDefaultHostname] == "") {
-					reqLogger.Info("Custom hostname has been removed from route, deleting and recreating the route")
-					route := &routev1.Route{ObjectMeta: defaultMeta}
-					err = r.DeleteResource(route)
-					if err != nil {
-						reqLogger.Error(err, "Failed to delete Route")
-						return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
-					}
+			if shouldDeleteRoute(ba) {
+				reqLogger.Info("Custom hostname has been removed from route, deleting and recreating the route")
+				route := &routev1.Route{ObjectMeta: defaultMeta}
+				err = r.DeleteResource(route)
+				if err != nil {
+					reqLogger.Error(err, "Failed to delete Route")
+					return r.ManageError(err, common.StatusConditionTypeReconciled, instance)
 				}
 			}
 
@@ -650,4 +640,20 @@ func (r *RuntimeComponentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
 func getMonitoringEnabledLabelName(ba common.BaseComponent) string {
 	return "monitor." + ba.GetGroupName() + "/enabled"
+}
+
+// If a custome hostname was previously set, but is now not set, any previous
+// route needs to be deleted, as the host in a route cannot be unset
+// and the default generated hostman is difficult to manually recreate
+func shouldDeleteRoute(ba common.BaseComponent) bool {
+	rh := ba.GetStatus().GetReferences()[common.StatusReferenceRouteHost]
+	if rh != "" {
+		// The host was previously set.
+		// If the host is now empty, delete the old route
+		rt := ba.GetRoute()
+		if rt == nil || (rt.GetHost() == "" && common.Config[common.OpConfigDefaultHostname] == "") {
+			return true
+		}
+	}
+	return false
 }
