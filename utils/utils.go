7ae55b900dcc247c558643d510838b43b7e2bedc
Leo Christy Jesuraj <leojc@ca.ibm.com>
2024-05-07 16:19:13 -0400

    Update operator version to 1.3.2
    Signed-off-by: Leo Christy Jesuraj <leojc@ca.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -41,7 +41,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-const RCOOperandVersion = "1.3.1"
+const RCOOperandVersion = "1.3.2"
 
 var APIVersionNotFoundError = errors.New("APIVersion is not available")
 
