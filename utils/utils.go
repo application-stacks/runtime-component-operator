414b7e8725fd32781a7eb05d7f14ebee989cf0f3
Leo Christy Jesuraj <leojc@ca.ibm.com>
2024-08-28 12:26:37 -0400

    Update RCO version to 1.4.0 and channel to v1.4
    Signed-off-by: Leo Christy Jesuraj <leojc@ca.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -41,7 +41,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-const RCOOperandVersion = "1.3.3"
+const RCOOperandVersion = "1.4.0"
 
 var APIVersionNotFoundError = errors.New("APIVersion is not available")
 
