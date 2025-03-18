9e877ae0b5f7aad288a84606efee92ef1c4d5922
Melissa Lee <halim.lee@ibm.com>
2025-03-18 12:46:59 -0400

    Update test cases

--- utils/status_test.go
+++ utils/status_test.go
@@ -23,76 +23,84 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
+var (
+	// Status test variables
+	st_replicas int32 = 3
+)
+
 func TestCheckApplicationStatus(t *testing.T) {
 	logger := zap.New()
 	logf.SetLogger(logger)
 
 	// Setup fake client and reconciler base
-	replicas := int32(3)
-	spec = appstacksv1.RuntimeComponentSpec{Replicas: &replicas}
-	runtimecomponent := createRuntimeComponent(name, namespace, spec)
-	objs, s := []runtime.Object{runtimecomponent}, scheme.Scheme
-	s.AddKnownTypes(appstacksv1.GroupVersion, runtimecomponent)
-	cl := fakeclient.NewFakeClient(objs...)
-	rcl := fakeclient.NewFakeClient(objs...)
+	spec := appstacksv1.RuntimeComponentSpec{Replicas: &st_replicas}
+	r, runtimecomponent := setupFakeClientWithRC(spec)
 
-	r := NewReconcilerBase(rcl, cl, s, &rest.Config{}, record.NewFakeRecorder(10))
-
-	// Overall application status should return ApplicationNotReconciled
+	// Overall application status should report ApplicationNotReconciled with no successful reconciliation
 	_, newCondition := r.CheckApplicationStatus(runtimecomponent)
 	notReconciled := newCondition.GetReason()
 
-	// Test Deployment resource status check
-	dpNotCreated, dpReplicasUnavailable, dpReady := testDeploymentReplicasReady(r, runtimecomponent)
+	// Create Deployment with less number of ready replicas
+	// ResourcesReady condition should report MinimumReplicasUnavailable with less number of ready replicas
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Status: appsv1.DeploymentStatus{
+			Replicas:        3,
+			ReadyReplicas:   3,
+			UpdatedReplicas: 3,
+		},
+	}
+	r.CreateOrUpdate(deploy, runtimecomponent, func() error {
+		CustomizeDeployment(deploy, runtimecomponent)
+		return nil
+	})
 
-	// Test StatefulSet resource status check
-	ssNotCreated, ssReplicasUnavailable, ssReady := testStatefulSetReplicasReady(r, runtimecomponent)
-
-	// Test Knative resource status check
-	ksNotCreated, ksStatusNotCreated, ksServiceNotReady, ksServiceReadyMsg := testKnativeReady(r, runtimecomponent)
+	// Run successful reconciliation and check ready application status
+	r.ManageSuccess(common.StatusConditionTypeReconciled, runtimecomponent)
+	_, newCondition = r.CheckApplicationStatus(runtimecomponent)
+	reconciledMessage := newCondition.GetMessage()
 
 	testAS := []Test{
 		{test: "Not reconciled", expected: "ApplicationNotReconciled", actual: notReconciled},
-		{test: "Deployment not created", expected: "NotCreated", actual: dpNotCreated},
-		{test: "Deployment replicas unavailable", expected: "MinimumReplicasUnavailable", actual: dpReplicasUnavailable},
-		{test: "Deployment ready", expected: "MinimumReplicasAvailable", actual: dpReady},
-		{test: "StatefulSet not created", expected: "NotCreated", actual: ssNotCreated},
-		{test: "StatefulSet replicas unavailable", expected: "MinimumReplicasUnavailable", actual: ssReplicasUnavailable},
-		{test: "StatefulSet ready", expected: "MinimumReplicasAvailable", actual: ssReady},
-		{test: "Knative not created", expected: "ServiceNotCreated", actual: ksNotCreated},
-		{test: "Knative Status not created", expected: "ServiceStatusNotFound", actual: ksStatusNotCreated},
-		{test: "Knative Status not ready", expected: "ServiceNotReady", actual: ksServiceNotReady},
-		{test: "Knative Status ready", expected: "Knative service is ready.", actual: ksServiceReadyMsg},
+		{test: "Reconciled and ready", expected: "Application is reconciled and resources are ready.", actual: reconciledMessage},
 	}
 
 	verifyTests(testAS, t)
 }
 
-// Partial test for areReplicasReady
-func testDeploymentReplicasReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeComponent) (string, string, string) {
+// Test areReplicasReady for Deployment resource status check
+func TestDeploymentReplicasReady(t *testing.T) {
 
-	// Report successful reconciliation
-	// Overall application status should return NotCreated with no resource created.
+	// Setup fake client and reconciler base
+	// Set RuntimeComponent to use Deployment
+	spec = appstacksv1.RuntimeComponentSpec{Replicas: &st_replicas}
+	r, runtimecomponent := setupFakeClientWithRC(spec)
+
+	// Report successful reconciliation to check for ResourcesReady condition
 	r.ManageSuccess(common.StatusConditionTypeReconciled, runtimecomponent)
 	conditionType := common.StatusConditionTypeResourcesReady
 	newCondition := runtimecomponent.GetStatus().NewCondition(conditionType)
 
+	// ResourcesReady condition should report NotCreated with no Deployment created
 	resourceCondition := r.areReplicasReady(runtimecomponent, newCondition)
 	dpNotCreated := resourceCondition.GetReason()
 
-	objMeta := metav1.ObjectMeta{
-		Name:      name,
-		Namespace: namespace,
-	}
+	// Create Deployment with less ready replicas than expected
+	// ResourcesReady condition should report MinimumReplicasUnavailable
 	deploy := &appsv1.Deployment{
-		ObjectMeta: objMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
 		Status: appsv1.DeploymentStatus{
 			Replicas:        3,
 			ReadyReplicas:   1,
 			UpdatedReplicas: 1,
 		},
 	}
-
 	r.CreateOrUpdate(deploy, runtimecomponent, func() error {
 		CustomizeDeployment(deploy, runtimecomponent)
 		return nil
@@ -101,51 +109,77 @@ func testDeploymentReplicasReady(r ReconcilerBase, runtimecomponent *appstacksv1
 	resourceCondition = r.areReplicasReady(runtimecomponent, newCondition)
 	dpReplicasUnavailable := resourceCondition.GetReason()
 
+	// Update Deployment with all more replicas than expected
+	// ResourcesReady condition should report ReplicaSetUpdating
+	deploy.Status = appsv1.DeploymentStatus{
+		Replicas:        4,
+		ReadyReplicas:   3,
+		UpdatedReplicas: 3,
+	}
+	r.GetClient().Status().Update(context.Background(), deploy)
+
+	resourceCondition = r.areReplicasReady(runtimecomponent, newCondition)
+	dpReplicaSetUpdating := resourceCondition.GetReason()
+
+	// Update Deployment with all ready replicas
+	// ResourcesReady condition should report MinimumReplicasAvailable
 	deploy.Status = appsv1.DeploymentStatus{
 		Replicas:        3,
 		ReadyReplicas:   3,
 		UpdatedReplicas: 3,
 	}
-
 	r.GetClient().Status().Update(context.Background(), deploy)
 
 	resourceCondition = r.areReplicasReady(runtimecomponent, newCondition)
 	dpReady := resourceCondition.GetReason()
 
-	return dpNotCreated, dpReplicasUnavailable, dpReady
+	// Test Deployment resource status
+	testDR := []Test{
+		{test: "Deployment not created", expected: "NotCreated", actual: dpNotCreated},
+		{test: "Deployment replicas unavailable", expected: "MinimumReplicasUnavailable", actual: dpReplicasUnavailable},
+		{test: "Deployment ReplicaSet updating", expected: "ReplicaSetUpdating", actual: dpReplicaSetUpdating},
+		{test: "Deployment ready", expected: "MinimumReplicasAvailable", actual: dpReady},
+	}
+
+	verifyTests(testDR, t)
 }
 
-// Partial test for areReplicasReady
-func testStatefulSetReplicasReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeComponent) (string, string, string) {
-	runtimecomponent.Spec.StatefulSet = &appstacksv1.RuntimeComponentStatefulSet{}
+// Test areReplicasReady for StatefulSet resource status check
+func TestStatefulSetReplicasReady(t *testing.T) {
+	// Setup fake client and reconciler base
+	// Set RuntimeComponent to use StatefulSet
+	spec = appstacksv1.RuntimeComponentSpec{Replicas: &st_replicas, StatefulSet: &appstacksv1.RuntimeComponentStatefulSet{}}
+	r, runtimecomponent := setupFakeClientWithRC(spec)
 
-	// Report successful reconciliation
-	// Overall application status should return NotCreated with no resource created.
+	// Report successful reconciliation to check for ResourcesReady condition
 	r.ManageSuccess(common.StatusConditionTypeReconciled, runtimecomponent)
 	conditionType := common.StatusConditionTypeResourcesReady
 	newCondition := runtimecomponent.GetStatus().NewCondition(conditionType)
 
+	// ResourcesReady condition should report NotCreated with no StatefulSet created
 	resourceCondition := r.areReplicasReady(runtimecomponent, newCondition)
 	ssNotCreated := resourceCondition.GetReason()
 
-	objMeta := metav1.ObjectMeta{
-		Name:      name,
-		Namespace: namespace,
-	}
+	// Create StatefulSet with less ready replicas than expected
+	// ResourcesReady condition should report MinimumReplicasUnavailable
 	statefulset := &appsv1.StatefulSet{
-		ObjectMeta: objMeta,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
 		Status: appsv1.StatefulSetStatus{
 			Replicas:        3,
 			ReadyReplicas:   1,
 			UpdatedReplicas: 1,
 		},
 	}
-
 	r.CreateOrUpdate(statefulset, runtimecomponent, func() error {
 		CustomizeStatefulSet(statefulset, runtimecomponent)
 		return nil
 	})
 
+	// Update StatefulSet with all ready replicas
+	// ResourcesReady condition should report MinimumReplicasAvailable with all ready replicas
 	resourceCondition = r.areReplicasReady(runtimecomponent, newCondition)
 	ssReplicasUnavailable := resourceCondition.GetReason()
 
@@ -159,30 +193,42 @@ func testStatefulSetReplicasReady(r ReconcilerBase, runtimecomponent *appstacksv
 	resourceCondition = r.areReplicasReady(runtimecomponent, newCondition)
 	ssReady := resourceCondition.GetReason()
 
-	return ssNotCreated, ssReplicasUnavailable, ssReady
+	// Test StatefulSet resource status
+	testSR := []Test{
+		{test: "StatefulSet not created", expected: "NotCreated", actual: ssNotCreated},
+		{test: "StatefulSet replicas unavailable", expected: "MinimumReplicasUnavailable", actual: ssReplicasUnavailable},
+		{test: "StatefulSet ready", expected: "MinimumReplicasAvailable", actual: ssReady},
+	}
+
+	verifyTests(testSR, t)
 }
 
-// Partial test for isKnativeReady
-func testKnativeReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeComponent) (string, string, string, string) {
-	createKnativeService := true
-	runtimecomponent.Spec.CreateKnativeService = &createKnativeService
+// Test isKnativeReady for Knative resource status check
+func TestKnativeReady(t *testing.T) {
 
-	// Report successful reconciliation
-	// Overall application status should return NotCreated with no resource created.
+	// Setup fake client and reconciler base
+	// Set RuntimeComponent to use KnativeService
+	createKnativeService := true
+	spec = appstacksv1.RuntimeComponentSpec{Replicas: &st_replicas, CreateKnativeService: &createKnativeService}
+	r, runtimecomponent := setupFakeClientWithRC(spec)
+
+	// Report successful reconciliation to check for ResourcesReady condition
 	r.ManageSuccess(common.StatusConditionTypeReconciled, runtimecomponent)
 	conditionType := common.StatusConditionTypeResourcesReady
 	newCondition := runtimecomponent.GetStatus().NewCondition(conditionType)
 
+	// ResourcesReady condition should report ServiceNotCreated with no KnativeService created
 	resourceCondition := r.isKnativeReady(runtimecomponent, newCondition)
 	ksNotCreated := resourceCondition.GetReason()
 
+	// Create KnativeService and add the type to the fake client
+	// ResourcesReady condition should report ServiceStatusNotFound with empty KnativeService status
 	knative1 := &servingv1.Service{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
 	}
-
 	r.scheme.AddKnownTypes(schema.GroupVersion{Group: "serving.knative.dev", Version: "v1"}, knative1)
 	r.CreateOrUpdate(knative1, runtimecomponent, func() error {
 		return nil
@@ -190,8 +236,10 @@ func testKnativeReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeCom
 	resourceCondition = r.isKnativeReady(runtimecomponent, newCondition)
 	ksStatusNotCreated := resourceCondition.GetReason()
 
-	// Unable to update status for Knative through fake client
-	// Deleting and re-creating Knative service with status conditions
+	// Unable to update status for KnativeService through fake client because it is not a known type
+	// Deleting and re-creating KnativeService to update its status conditions
+	// Update KnativeService with Not Ready condition
+	// ResourcesReady condition should report ServiceNotReady
 	r.DeleteResource(knative1)
 
 	knative2 := &servingv1.Service{
@@ -200,7 +248,6 @@ func testKnativeReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeCom
 			Namespace: namespace,
 		},
 	}
-
 	condition := apis.Condition{
 		Type:   apis.ConditionReady,
 		Status: v1.ConditionFalse,
@@ -216,6 +263,9 @@ func testKnativeReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeCom
 	resourceCondition = r.isKnativeReady(runtimecomponent, newCondition)
 	ksServiceNotReady := resourceCondition.GetReason()
 
+	// Update KnativeService with Ready condition
+	// There is no reason field reported - using message instead for this case
+	// ResourcesReady condition should report "Knative service is ready." message
 	r.DeleteResource(knative2)
 
 	knative3 := &servingv1.Service{
@@ -240,5 +290,26 @@ func testKnativeReady(r ReconcilerBase, runtimecomponent *appstacksv1.RuntimeCom
 	resourceCondition = r.isKnativeReady(runtimecomponent, newCondition)
 	ksServiceReadyMsg := resourceCondition.GetMessage()
 
-	return ksNotCreated, ksStatusNotCreated, ksServiceNotReady, ksServiceReadyMsg
+	// Test KnatvieService resource status
+	testKS := []Test{
+		{test: "Knative not created", expected: "ServiceNotCreated", actual: ksNotCreated},
+		{test: "Knative Status not created", expected: "ServiceStatusNotFound", actual: ksStatusNotCreated},
+		{test: "Knative Status not ready", expected: "ServiceNotReady", actual: ksServiceNotReady},
+		{test: "Knative Status ready", expected: "Knative service is ready.", actual: ksServiceReadyMsg},
+	}
+
+	verifyTests(testKS, t)
+}
+
+// Setup fake client with RuntimeComponent kind
+// Creates a RuntimeComponent object with input spec
+func setupFakeClientWithRC(spec appstacksv1.RuntimeComponentSpec) (ReconcilerBase, *appstacksv1.RuntimeComponent) {
+	runtimecomponent := createRuntimeComponent(name, namespace, spec)
+	objs, s := []runtime.Object{runtimecomponent}, scheme.Scheme
+	s.AddKnownTypes(appstacksv1.GroupVersion, runtimecomponent)
+	cl := fakeclient.NewFakeClient(objs...)
+	rcl := fakeclient.NewFakeClient(objs...)
+	r := NewReconcilerBase(rcl, cl, s, &rest.Config{}, record.NewFakeRecorder(10))
+
+	return r, runtimecomponent
 }
