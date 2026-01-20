9f55eb26b27670ab32b04805edfa3747459b3638
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2026-01-20 16:31:01 -0500

    Add .spec.service.disableTopology to clear topology annotations

--- api/v1/runtimecomponent_types.go
+++ api/v1/runtimecomponent_types.go
@@ -317,6 +317,10 @@ type RuntimeComponentService struct {
 	// Configure service session affinity.
 	// +operator-sdk:csv:customresourcedefinitions:order=19,type=spec
 	SessionAffinity *RuntimeComponentServiceSessionAffinity `json:"sessionAffinity,omitempty"`
+
+	// Disables topology aware annotations from being added to the Service. Defaults to false.
+	// +operator-sdk:csv:customresourcedefinitions:order=20,type=spec,displayName="Disable Topology",xDescriptors="urn:alm:descriptor:com.tectonic.ui:booleanSwitch"
+	DisableTopology *bool `json:"disableTopology,omitempty"`
 }
 
 // Configure service session affinity
@@ -777,6 +781,13 @@ func (cr *RuntimeComponent) GetManagedScheme() corev1.URIScheme {
 		return corev1.URISchemeHTTPS
 	}
 	return corev1.URISchemeHTTP
+}
+
+func (cr *RuntimeComponent) GetDisableTopology() *bool {
+	if cr.Spec.Service != nil {
+		return cr.Spec.Service.DisableTopology
+	}
+	return nil
 }
 
 // GetDeployment returns deployment settings
