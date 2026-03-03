b1de5861e077e0f2a0982ffdbc191f34e5c59c0f
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2026-03-03 14:42:10 -0500

    Support multiple pull secrets (#742)
    * Add .spec.pullSecrets for using multiple secrets
    
    * Revert .spec.pullSecrets and merge impl into .spec.pullSecret
    
    * Add DecodeStringToList and EncodeListToString helpers
    
    * Pull bundle changes from main and regenerate bundle

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 	"sort"
 	"strconv"
 	"strings"
@@ -874,48 +875,65 @@ func CustomizeServiceAccount(sa *corev1.ServiceAccount, ba common.BaseComponent,
 	sa.Annotations = MergeMaps(sa.Annotations, ba.GetAnnotations())
 
 	psr := ba.GetStatus().GetReferences()[common.StatusReferencePullSecretName]
+	pullSecrets := Set(DecodeStringToList(psr))
+	crPullSecrets := []string{}
+	if ba.GetPullSecret() != nil {
+		crPullSecrets = Set(DecodeStringToList(*ba.GetPullSecret()))
+	}
 	if psr != "" && (ba.GetPullSecret() == nil || *ba.GetPullSecret() != psr) {
 		// There is a reference to a pull secret but it doesn't match the one
 		// from the CR (which is empty or different)
 		// so delete the refered pull secret from the service account
-		removePullSecret(sa, psr)
+		missingPullSecrets := SetDiff(pullSecrets, crPullSecrets)
+		removePullSecrets(sa, missingPullSecrets)
 	}
 
-	if ba.GetPullSecret() == nil {
+	if len(crPullSecrets) == 0 {
 		// There is no pull secret so delete the status reference
 		// This has to happen after the reference has been used to remove the pull
 		// secret from the service account
 		delete(ba.GetStatus().GetReferences(), common.StatusReferencePullSecretName)
 	} else {
-		// Add the pull secret from the CR to the service account. First check that it is valid
-		ps := *ba.GetPullSecret()
-		err := client.Get(context.TODO(), types.NamespacedName{Name: ps, Namespace: ba.(metav1.Object).GetNamespace()}, &corev1.Secret{})
-		if err != nil {
-			return err
-		}
-		ba.GetStatus().SetReference(common.StatusReferencePullSecretName, ps)
-		if len(sa.ImagePullSecrets) == 0 {
-			sa.ImagePullSecrets = append(sa.ImagePullSecrets, corev1.LocalObjectReference{
-				Name: ps,
-			})
-		} else {
-			pullSecretName := ps
-			found := false
-			for _, obj := range sa.ImagePullSecrets {
-				if obj.Name == pullSecretName {
-					found = true
-					break
-				}
+		// Add the pull secrets from the CR to the service account. First check that all are valid
+		for _, ps := range crPullSecrets {
+			err := client.Get(context.TODO(), types.NamespacedName{Name: ps, Namespace: ba.(metav1.Object).GetNamespace()}, &corev1.Secret{})
+			if err != nil {
+				return err
 			}
-			if !found {
+		}
+		ba.GetStatus().SetReference(common.StatusReferencePullSecretName, EncodeListToString(crPullSecrets))
+		if len(sa.ImagePullSecrets) == 0 {
+			for _, ps := range crPullSecrets {
 				sa.ImagePullSecrets = append(sa.ImagePullSecrets, corev1.LocalObjectReference{
-					Name: pullSecretName,
+					Name: ps,
 				})
+			}
+		} else {
+			for _, ps := range crPullSecrets {
+				pullSecretName := ps
+				found := false
+				for _, obj := range sa.ImagePullSecrets {
+					if obj.Name == pullSecretName {
+						found = true
+						break
+					}
+				}
+				if !found {
+					sa.ImagePullSecrets = append(sa.ImagePullSecrets, corev1.LocalObjectReference{
+						Name: pullSecretName,
+					})
+				}
 			}
 		}
 	}
 
 	return nil
+}
+
+func removePullSecrets(sa *corev1.ServiceAccount, pullSecrets []string) {
+	for _, ps := range pullSecrets {
+		removePullSecret(sa, ps)
+	}
 }
 
 func removePullSecret(sa *corev1.ServiceAccount, pullSecretName string) {
@@ -1988,4 +2006,55 @@ func parseEnvAsBool(envValue string) bool {
 		}
 	}
 	return false
+}
+
+// Encodes a list into a comma-separated string
+func EncodeListToString(arr []string) string {
+	return strings.Join(arr, ",")
+}
+
+// Decodes a comma-separated into a string list
+func DecodeStringToList(commaSeparatedString string) []string {
+	return strings.Split(string(commaSeparatedString), ",")
+}
+
+// Returns true if set A and B are equivalent
+func SetEquals(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	// track seen items in a
+	seen := map[string]bool{}
+	for _, v := range a {
+		seen[v] = true
+	}
+	// return false if b contains an element not in a
+	for _, v := range b {
+		if _, found := seen[v]; !found {
+			return false
+		}
+	}
+	return true
+}
+
+// Converts list A into a set without repeated values
+func Set(a []string) []string {
+	out := []string{}
+	for _, v := range a {
+		if !slices.Contains(out, v) {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+// Returns the set difference of A \ B
+func SetDiff(a []string, b []string) []string {
+	out := []string{}
+	for _, v := range a {
+		if !slices.Contains(b, v) {
+			out = append(out, v)
+		}
+	}
+	return out
 }
