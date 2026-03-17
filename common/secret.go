529c4cb99a67abb8a535c79bd0be836693c5d6d5
Kirby Chin <37311900+kabicin@users.noreply.github.com>
2026-03-17 13:37:50 -0400

    Use memguard locked buffer for Secret operations

--- common/secret.go
+++ common/secret.go
@@ -2,81 +2,92 @@ package common
 
 import (
 	"context"
-	"sync"
+	"fmt"
 
+	"github.com/awnumar/memguard"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type SecretResource struct {
-	secret *corev1.Secret
-	once   sync.Once
+type LockedBufferSecret struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	LockedData        SecretMap `json:"lockedData,omitempty"`
 }
 
-type WaitableSecretResource struct {
-	*SecretResource
-	wg sync.WaitGroup
-}
+type SecretMap map[string]*memguard.LockedBuffer
 
-// Creates a Secret that clears it's data when recCtx is canceled
-func NewSecret(recCtx context.Context, name, namespace string) *corev1.Secret {
-	secretResource := NewSecretResource(name, namespace)
-	go func() {
-		<-recCtx.Done()
-		secretResource.Clear(nil)
-	}()
-	return secretResource.GetSecret()
-}
-
-func NewWaitableSecret(recCtx context.Context, name, namespace string) (*corev1.Secret, *sync.WaitGroup) {
-	waitableSecretResource := NewWaitableSecretResource(name, namespace)
-	go func() {
-		<-recCtx.Done()
-		waitableSecretResource.Clear(waitableSecretResource.GetWaitGroup())
-	}()
-	return waitableSecretResource.GetSecret(), waitableSecretResource.GetWaitGroup()
-}
-
-func NewSecretResource(name, namespace string) *SecretResource {
-	resource := &SecretResource{
-		secret: &corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      name,
-				Namespace: namespace,
-			},
-		},
+func (sm SecretMap) Destroy() {
+	for _, buf := range sm {
+		buf.Destroy()
 	}
-	return resource
 }
 
-func NewWaitableSecretResource(name, namespace string) *WaitableSecretResource {
-	resource := &WaitableSecretResource{
-		SecretResource: NewSecretResource(name, namespace),
-		wg:             sync.WaitGroup{},
+func (sm SecretMap) Get(key string) ([]byte, bool) {
+	if buf, found := sm[key]; found {
+		return buf.Bytes(), true
 	}
-	return resource
+	return []byte{}, false
 }
 
-func (wsr *WaitableSecretResource) GetWaitGroup() *sync.WaitGroup {
-	return &wsr.wg
+func (lockedBufferSecret LockedBufferSecret) Destroy() {
+	if lockedBufferSecret.LockedData != nil {
+		lockedBufferSecret.LockedData.Destroy()
+	}
 }
 
-func (r *SecretResource) GetSecret() *corev1.Secret {
-	return r.secret
-}
-
-func (r *SecretResource) Clear(wg *sync.WaitGroup) {
-	if r.secret == nil {
-		return
+// Gets a Secret from the k8s client loaded as a LockedBufferSecret
+func GetSecret(client client.Client, name string, ns string) (*LockedBufferSecret, error) {
+	if client == nil {
+		return nil, fmt.Errorf("the reconciler client could not be found")
+	}
+	secret := &corev1.Secret{}
+	secret.Name = name
+	secret.Namespace = ns
+	err := client.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: ns}, secret)
+	if err != nil {
+		return nil, err
 	}
 
-	r.once.Do(func() {
-		if wg != nil {
-			wg.Wait()
+	lockedBufferSecret := &LockedBufferSecret{}
+	lockedBufferSecret.TypeMeta = secret.TypeMeta
+	lockedBufferSecret.ObjectMeta = secret.ObjectMeta
+	for secretKey, secretValue := range secret.Data {
+		lockedBufferSecret.LockedData[secretKey] = memguard.NewBufferFromBytes(secretValue)
+	}
+	return lockedBufferSecret, nil
+}
+
+// Copies a Locked Buffer Secret into a core Secret with a corresponding cleanup func
+func CopySecret(in *LockedBufferSecret, out *corev1.Secret) func() {
+	out.TypeMeta = in.TypeMeta
+	out.ObjectMeta = in.ObjectMeta
+	for key, buf := range in.LockedData {
+		out.Data[key] = buf.Bytes()
+	}
+	return func() {
+		for key := range out.Data {
+			delete(out.Data, key)
 		}
-		for secretKey, secretValue := range r.secret.Data {
-			clear(secretValue)
-			delete(r.secret.Data, secretKey)
-		}
-	})
+		out.Data = nil
+	}
+}
+
+// Returns the client.Get status of Secret name in namespace ns after clearing sensitive byte array content
+func CheckSecret(client client.Client, name string, ns string) error {
+	if client == nil {
+		return fmt.Errorf("the reconciler client could not be found")
+	}
+
+	secret := &corev1.Secret{}
+	secret.Name = name
+	secret.Namespace = ns
+	err := client.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: ns}, secret)
+	for key, value := range secret.Data {
+		clear(value)
+		delete(secret.Data, key)
+	}
+	return err
 }
